fd8b0ecf3e831fa82aa5f1e5e5077ea685f66051
Adeoye1996 <abdulazeezbabatunde10@gmail.com>
2024-10-10 16:27:51 +0100

    update

--- school-website/src/index.js
+++ school-website/src/index.js
@@ -1,17 +1,7 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
+import App from './App'; // Adjust the path if your App component is located elsewhere
+// import './index.css'; // Comment this out if you don't have index.css
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const rootElement = document.getElementById('root');
+ReactDOM.render(<App />, rootElement);
